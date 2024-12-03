f8e6dd21102f6ccd4af4865c59c08c7cdc5f95d5
stud0000254289 <stud0000254289@study.utmn.ru>
2024-12-03 20:38:06 +0500

    Очистка ветки lab5

--- database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function getDatabaseConnection(): PDO {
-    $db = new PDO('sqlite:C:/OSPanel/domains/my_project/databases/ITBlab4db'); 
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $db;
-}
-
